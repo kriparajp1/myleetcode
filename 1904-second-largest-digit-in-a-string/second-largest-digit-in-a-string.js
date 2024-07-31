add6e4019db64ea3f7f42be1a95ee37949d1a78c
kriparaj P <156789243+kriparajp1@users.noreply.github.com>
2024-07-31 16:43:38 +0530

    Time: 71 ms (18.86%) | Memory: 51 MB (30.86%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.js
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.js
@@ -5,7 +5,7 @@
 var secondHighest = function(s) {
     let l=s.split("")
     let m=l.filter((a,b)=>!isNaN(+a)).map(a=>+a)
-    let j=Array.from(new Set(m)).sort((a,b)=>b-a)
-    return j.length<2?-1:j[1]
+    let j=Array.from(new Set(m)).sort((a,b)=>a-b)
+    return j.length<2?-1:j[j.length-2]
     
 };
